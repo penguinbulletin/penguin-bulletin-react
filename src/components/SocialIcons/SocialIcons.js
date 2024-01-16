4c3a8286adb2883f714738d79c0b499508c3c194
Penguin Reno <penguinbulletin@gmail.com>
2024-01-16 22:07:58 +1100

    added more social icons and links

--- src/components/SocialIcons/SocialIcons.js
+++ src/components/SocialIcons/SocialIcons.js
@@ -4,6 +4,8 @@ import { ReactComponent as DiscordSVG } from './icon-discord.svg';
 import { ReactComponent as SteamSVG } from './icon-steam.svg';
 import { ReactComponent as LinkedInSVG } from './icon-linkedin.svg';
 import { ReactComponent as YoutubeSVG } from './icon-youtube.svg';
+import { ReactComponent as GithubSVG } from './icon-github.svg';
+import { ReactComponent as KofiSVG } from './icon-kofi.svg';
 
 const backgroundColor = "#282c34";
 const strokeColor= "white";
@@ -11,17 +13,23 @@ const strokeColor= "white";
 function SocialIcons() {
   return (
     <div className="social-bar">
-      <a href="google.com">
+      <a href="mailto:reno@penguinbulletin.com">
         <svg className="social-icon"><MailSVG stroke={strokeColor} fill={backgroundColor}> </MailSVG></svg>
       </a>
-      <a href="google.com">
+      <a href="https://discord.gg/wDMrCQRn">
+        <svg className="social-icon"><DiscordSVG stroke={strokeColor} fill={backgroundColor}> </DiscordSVG></svg>
+      </a>
+      <a href="https://github.com/penguinbulletin">
+        <svg className="social-icon"><GithubSVG stroke={strokeColor} fill={backgroundColor}> </GithubSVG></svg>
+      </a>
+      <a href="https://www.youtube.com/channel/UC1rBXReOE_l75YliTRfKMfg">
         <svg className="social-icon"><YoutubeSVG stroke={strokeColor} fill={backgroundColor}> </YoutubeSVG></svg>
       </a>
-      <a href="google.com">
+      <a href="https://www.linkedin.com/in/jarren-ong/">
         <svg className="social-icon"><LinkedInSVG stroke={strokeColor} fill={backgroundColor}> </LinkedInSVG></svg>
       </a>
-      <a href="google.com">
-        <svg className="social-icon"><DiscordSVG stroke={strokeColor} fill={backgroundColor}> </DiscordSVG></svg>
+      <a href="https://ko-fi.com/penguinbulletin">
+        <svg className="social-icon"><KofiSVG stroke={strokeColor} fill={backgroundColor}> </KofiSVG></svg>
       </a>
       {/* <a href="google.com">
         <svg className="social-icon"><SteamSVG stroke={strokeColor} fill={backgroundColor}> </SteamSVG></svg>
